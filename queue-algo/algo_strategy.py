b20ed85a11886f47f286acdfaf12f2d54d6a2974
Ian Haggerty <ianhaggerty3@gmail.com>
2020-10-05 16:42:58 -0400

    Reformated algo to hopefully make it easier to change stuff

--- queue-algo/algo_strategy.py
+++ queue-algo/algo_strategy.py
@@ -1,13 +1,12 @@
 from gamelib import AlgoCore, debug_write, GameState, GameUnit
-from enum import Enum
 import random
 import math
 import warnings
 from sys import maxsize
 import json
 
-from typing import Dict, List, Set
-
+from typing import Dict, List, Set, Union
+from utility import Utility
 
 """
 Most of the algo code you write will be in this file unless you create new
@@ -26,18 +25,19 @@ Advanced strategy tips:
 class AlgoStrategy(AlgoCore):
     cached_health: int
     health: int
-    all_locations: Set[int]
-    ARENA_SIZE = 28
+    utility: Utility
 
     def __init__(self):
         super().__init__()
+        self.ARENA_SIZE = 28
+
         seed = random.randrange(maxsize)
         random.seed(seed)
         debug_write('Random seed: {}'.format(seed))
-        self.edge_set = self.construct_edge_set
-        self.ARENA_SIZE = 28
+
         self.scored_on_locations = []
-        self.all_locations = set()
+
+        self.utility = Utility()
 
     def on_game_start(self, config):
         """ 
@@ -58,6 +58,7 @@ class AlgoStrategy(AlgoCore):
         SP = 0
 
         # This is a good place to do initial setup
+        self.add_initial_defence()
 
     def on_turn(self, turn_state):
         """
@@ -73,20 +74,14 @@ class AlgoStrategy(AlgoCore):
             self.cached_health = self.health
 
         debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        game_state.suppress_warnings(True)
 
-        self.starter_strategy(game_state)
+        self.dynamic_strategy(game_state)
         self.cached_health = self.health
 
         game_state.submit_turn()
 
-
-    """
-    NOTE: All the methods after this point are part of the sample starter-algo
-    strategy and can safely be replaced for your custom algo.
-    """
-
-    def starter_strategy(self, game_state: GameState):
+    def dynamic_strategy(self, game_state: GameState):
         """
         For defense we will use a spread out layout and some interceptors early on.
         We will place turrets near locations the opponent managed to score on.
@@ -94,103 +89,64 @@ class AlgoStrategy(AlgoCore):
         If there are no stationary units to attack in the front, we will send Scouts to try and score quickly.
         """
         # First, place basic defenses
-        self.build_defences(game_state)
+        # self.build_defences(game_state)
         # Now build reactive defenses based on where the enemy scored
         if self.cached_health != self.health:
-            self.build_reactive_defense(game_state)
+            self.build_reactive_defense()
 
         # If the turn is less than 5, stall with interceptors and wait to see enemy's base
-        if game_state.turn_number < 5:
+        if game_state.turn_number < 3:
             self.stall_with_interceptors(game_state)
-        else:
+        if game_state.turn_number == 3:
+            factory_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
+            self.utility.append_action('extra_factories', FACTORY, factory_locations)
+        if game_state.turn_number >= 3:
             # Now let's analyze the enemy base to see where their defenses are concentrated.
             # If they have many units in the front we can build a line for our demolishers to attack them at long range.
-            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
-                self.demolisher_line_strategy(game_state)
-            else:
-                # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
+            # if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
+            #     self.demolisher_line_strategy(game_state)
+            # else:
+            # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
 
-                # Only spawn Scouts every other turn
-                # Sending more at once is better since attacks can only hit a single scout at a time
-                if game_state.turn_number % 2 == 1:
-                    # To simplify we will just check sending them from back left and right
-                    scout_spawn_location_options = [[10, 3], [17, 3]]
-                    try:
-                        best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
-                        game_state.attempt_spawn(SCOUT, best_location, 1000)
-                    except ValueError:
-                        debug_write('could not find a path for spawning')
-
-                # Lastly, if we have spare SP, let's build some Factories to generate more resources
-                factory_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
-                self.add_locations(factory_locations)
-
-                game_state.attempt_spawn(FACTORY, factory_locations)
+            # Only spawn Scouts every other turn
+            # Sending more at once is better since attacks can only hit a single scout at a time
+            if game_state.turn_number % 2 == 1:
+                # To simplify we will just check sending them from back left and right
+                scout_spawn_location_options = [[10, 3], [17, 3]]
+                try:
+                    best_location = self.least_damage_spawn_location(game_state, scout_spawn_location_options)
+                    game_state.attempt_spawn(SCOUT, best_location, 1000)
+                except ValueError:
+                    debug_write('could not find a path for spawning')
 
         # always try to use more resources
-        self.upgrade_all(game_state)
-
-    def add_locations(self, locations: List[List[int]]):
-        for location in locations:
-            self.all_locations.add(self.point_hash(location))
+        self.utility.attempt_actions(game_state)
 
     def upgrade_all(self, game_state: GameState):
         for location_hash in self.all_locations:
-            location = self.inv_point_hash(location_hash)
+            location = self.utility.inv_point_hash(location_hash)
             if game_state.attempt_upgrade(location) == 0:
                 break
 
-    def attempt_spawn_refresh(self, game_state: GameState, unit_type, locations: List[List[int]], threshold=0.75):
+    def wall_surround(self, locations: List[List[int]]):
         """
-        Does the same thing as attempt_spawn, but will automatically sell and repurchase units below a health threshold
+        Surround the passed spot with walls, including the spot itself if empty
         """
-        for location in locations:
-            if len(game_state.game_map[location]):
-                unit: GameUnit = game_state.game_map[location[0], location[1]][0]
-                if unit.health <= int(unit.max_health * threshold):
-                    game_state.attempt_remove(location)
-                    game_state.attempt_spawn(unit_type, location)
-                    if unit.upgraded:
-                        game_state.attempt_upgrade(location)
-                        debug_write("[DEBUG] replacing upgraded unit")
-                    else:
-                        debug_write("[DEBUG] replacing normal unit")
-            else:
-                game_state.attempt_spawn(unit_type, location)
-
-    def point_hash(self, location: List[int]) -> int:
-        """
-        Since lists aren't hashable, this maps each point on the grid to a unique int, for adding to a collection
-        """
-        return 1 * location[0] + self.ARENA_SIZE * location[1]
-
-    def inv_point_hash(self, hash: int):
-        return [hash % self.ARENA_SIZE, hash // self.ARENA_SIZE]
-
-    @property
-    def construct_edge_set(self) -> Set[int]:
-        ret = set()
-        for x in range(14):
-            ret.add(self.point_hash([x, 13 - x]))
-            ret.add(self.point_hash([27 - x, 13 - x]))
-
-        return ret
-
-    def on_edge(self, location: List[int]):
-        return self.point_hash(location) in self.edge_set
-
-    def wall_surround(self, game_state: GameState, locations: List[List[int]]):
         new_locations = []
         for location in locations:
-            new_locations += list(filter(
-                # don't want to spawn walls where they could block mobile unit spawns, in most cases
-                lambda entry: not self.on_edge(entry),
-                [[location[0] + 1, location[1]], [location[0] - 1, location[1]], [location[0], location[1] + 1]]
-            ))
+            # we might eventually want to block where we spawn walls, but for now this logic isn't good enough; it can
+            # still block useful paths, but prevents us from putting walls in some much-needed locations
+            # new_locations += list(filter(
+            #     lambda entry: not self.on_edge(entry),
+            #     [[location[0], location[1]], [location[0] + 1, location[1]],
+            #      [location[0] - 1, location[1]], [location[0], location[1] + 1]]
+            # ))
+            new_locations += [[location[0], location[1]], [location[0] + 1, location[1]],
+                              [location[0] - 1, location[1]], [location[0], location[1] + 1]]
 
-        game_state.attempt_spawn(WALL, new_locations)
+        self.utility.append_action('surround_walls' + str(len(locations)), WALL, new_locations)
 
-    def build_defences(self, game_state: GameState):
+    def add_initial_defence(self) -> None:
         """
         Build basic defenses using hardcoded locations.
         Remember to defend corners and avoid placing units in the front where enemy demolishers can attack them.
@@ -198,33 +154,36 @@ class AlgoStrategy(AlgoCore):
         # Useful tool for setting up your base locations: https://www.kevinbai.design/terminal-map-maker
         # More community tools available at: https://terminal.c1games.com/rules#Download
 
-        # Place turrets that attack enemy units
+        # Add default turrets
         turret_locations = [[3, 12], [24, 12], [8, 11], [19, 11], [13, 11], [14, 11]]
-        self.add_locations(turret_locations)
-        # attempt_spawn will try to spawn units if we have resources, and will check if a blocking unit is already there
-        self.attempt_spawn_refresh(game_state, TURRET, turret_locations)
-        game_state.attempt_upgrade([[3, 12], [24, 12]])
-        self.wall_surround(game_state, turret_locations)
-        
+        self.utility.append_action('initial_turrets', TURRET, turret_locations)
+
+        initial_upgrades = [[3, 12], [24, 12]]
+        self.utility.append_action('initial_upgrades', '', initial_upgrades, upgrade=True)
+
         # Place walls in front of turrets to soak up damage for them
-        wall_locations = [[0, 13], [1, 13], [1, 12], [27, 13], [26, 13], [26, 12]]
-        self.add_locations(wall_locations)
-        game_state.attempt_spawn(WALL, wall_locations)
+        self.wall_surround(turret_locations)
+
+        # wall_locations = [[0, 13], [1, 13], [1, 12], [27, 13], [26, 13], [26, 12]]
+        # self.utility.append_action('initial_walls', WALL, wall_locations)
 
         factory_locations = [[13, 0], [14, 0]]
-        self.add_locations(factory_locations)
-        self.attempt_spawn_refresh(game_state, FACTORY, factory_locations)
+        self.utility.append_action('initial_factories', FACTORY, factory_locations)
 
-    def build_reactive_defense(self, game_state: GameState):
+    def build_reactive_defense(self):
         """
         This function builds reactive defenses based on where the enemy scored on us from.
         We can track where the opponent scored by looking at events in action frames 
         as shown in the on_action_frame function
         """
-        for location in self.scored_on_locations:
-            # Build turret one space above so that it doesn't block our own edge spawn locations
-            build_location = [location[0], location[1]+1]
-            game_state.attempt_spawn(TURRET, build_location)
+        self.wall_surround(self.scored_on_locations)
+
+        # don't really want to put turrets there
+        # for location in self.scored_on_locations:
+        #     # Build turret one space above so that it doesn't block our own edge spawn locations
+        #     # build_location = [location[0], location[1]+1]
+        #     # game_state.attempt_spawn(TURRET, build_location)
+        #     self.wall_surround(self.scored_on_locations)
 
     def stall_with_interceptors(self, game_state: GameState):
         """
@@ -303,7 +262,7 @@ class AlgoStrategy(AlgoCore):
                     if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
                         total_units += 1
         return total_units
-        
+
     def filter_blocked_locations(self, locations: List[List[int]], game_state: GameState):
         filtered = []
         for location in locations:
---
+++ queue-algo/utility.py
@@ -0,0 +1,146 @@
+from gamelib import debug_write, GameState, GameUnit
+from typing import Dict, List, Set, Union
+
+
+class Utility:
+    ARENA_SIZE: int
+
+    action_queue: List[Dict[str, Union[str, bool, List[List[int]]]]]
+    all_defenses: List[int]
+    change_flag: bool
+
+    def __init__(self):
+        self.ARENA_SIZE = 28
+
+        self.action_queue = []
+        self.all_defenses = []
+        self.edge_set = self.construct_edge_set
+        self.change_flag = False
+
+    def append_action(self, id: str, unit_type: str,
+                      locations: Union[List[int], List[List[int]]], upgrade=False) -> None:
+        """
+        Appends an action to the to the end of the priority queue
+        """
+        index = -1
+        for i, entry in enumerate(self.action_queue):
+            if entry['id'] == id:
+                index = i
+                break
+
+        if index != -1:
+            debug_write("[DEBUG] cannot append existing action:", id)
+            return
+
+        self.change_flag = True
+        location_list = [locations] if type(locations[0]) is int else locations
+        action_object = {'id': id, 'unit_type': unit_type, 'locations': location_list, 'upgrade': upgrade}
+        self.action_queue.append(action_object)
+
+    def prioritize_action(self, id: str) -> None:
+        """
+        Moves an action to the front of the priority queue by id
+        """
+        index = -1
+        for i, entry in enumerate(self.action_queue):
+            if entry['id'] == id:
+                index = i
+                break
+
+        if index == -1:
+            debug_write("[DEBUG] cannot prioritize nonexistant action:", id)
+            raise ValueError('Cannot prioritize nonexistant action')
+
+        self.change_flag = True
+        value = self.action_queue.pop(index)
+        self.action_queue.insert(0, value)
+
+    def remove_action(self, id: str) -> None:
+        """
+        Removes an action from the priority queue by id
+        """
+        index = -1
+        for i, entry in enumerate(self.action_queue):
+            if entry['id'] == id:
+                index = i
+                break
+
+        if index == -1:
+            debug_write("[DEBUG] cannot remove nonexistant action:", id)
+            raise ValueError('Cannot remove nonexistant action')
+
+        self.change_flag = True
+        remove_locations = self.action_queue[index].get('locations', [])
+        for location in remove_locations:
+            self.all_defenses.remove(self.point_hash(location))
+
+        self.action_queue.pop(index)
+
+    def attempt_actions(self, game_state: GameState):
+        """
+        Call on each turn to act on the finished action queue
+        Takes care of adding the updated upgrade_all action
+        """
+
+        if self.change_flag:
+            self.refresh_upgrade_all()
+
+        for action in self.action_queue:
+            if not action['upgrade']:
+                self.attempt_spawn_refresh(game_state, action['unit_type'], action['locations'])
+            else:
+                game_state.attempt_upgrade(action['locations'])
+
+    def refresh_upgrade_all(self):
+        try:
+            self.remove_action('upgrade_all')
+        except ValueError:
+            pass
+        self.all_defenses = []
+        for action in self.action_queue:
+            if action['upgrade'] is True:
+                continue
+            self.all_defenses += action['locations']
+
+        self.append_action('upgrade_all', '', self.all_defenses, upgrade=True)
+        self.change_flag = False
+
+    def attempt_spawn_refresh(self, game_state: GameState, unit_type: str, locations: List[List[int]], threshold=0.75):
+        """
+        Does the same thing as attempt_spawn, but will automatically sell and repurchase units below a health threshold
+        """
+        for location in locations:
+            if len(game_state.game_map[location[0], location[1]]):
+                unit: GameUnit = game_state.game_map[location[0], location[1]][0]
+                if unit.health <= int(unit.max_health * threshold):
+                    game_state.attempt_remove(location)
+                    game_state.attempt_spawn(unit_type, location)
+                    if unit.upgraded:
+                        game_state.attempt_upgrade(location)
+                        debug_write("[DEBUG] replacing upgraded unit")
+                    else:
+                        debug_write("[DEBUG] replacing normal unit")
+            else:
+                game_state.attempt_spawn(unit_type, location)
+
+    def point_hash(self, location: List[int]) -> int:
+        """
+        Since lists aren't hashable, this maps each point on the grid to a unique int, for adding to a collection
+        """
+        return 1 * location[0] + self.ARENA_SIZE * location[1]
+
+    def inv_point_hash(self, hashed_location: int):
+        return [hashed_location % self.ARENA_SIZE, hashed_location // self.ARENA_SIZE]
+
+    @property
+    def construct_edge_set(self) -> Set[int]:
+        ret = set()
+        for x in range(14):
+            ret.add(self.point_hash([x, 13 - x]))
+            ret.add(self.point_hash([27 - x, 13 - x]))
+
+        return ret
+
+    def on_edge(self, location: List[int]):
+        return self.point_hash(location) in self.edge_set
+
